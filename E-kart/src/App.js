54fedaba3707c18e525ac3903f3af6f49ef1d0b0
Rohan <rohan1agg@gmail.com>
2018-06-14 20:42:54 +0530

    e-kart

---
+++ E-kart/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Layout from './Components/Layout/Layout';
+//import logo from './logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
+
---
+++ E-kart/src/Components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
+const Layout = (props) => {
+	return(
+		<Aux>
+			<Toolbar />
+			<main className = {classes.Content}>{props.children}</main>
+		</Aux>
+	);
+}
+
+export default Layout;
